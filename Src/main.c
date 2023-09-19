e18165d8b109ee9b0631ae2a8db38aad8a996ba3
hyper-hronoz <vlad_moryakov_prog@mail.ru>
2023-09-19 15:58:07 +0300

    adding flash memory

--- Src/main.c
+++ Src/main.c
@@ -24,6 +24,50 @@
 // utils
 #include "delay.h"
 
+#define FLASH_KEY1 ((uint32_t)0x45670123)
+#define FLASH_KEY2 ((uint32_t)0xCDEF89AB)
+
+void flash_unlock(void) {
+  FLASH->KEYR = FLASH_KEY1;
+  FLASH->KEYR = FLASH_KEY2;
+}
+
+void flash_lock() { FLASH->CR |= FLASH_CR_LOCK; }
+
+uint8_t flash_ready(void) { return !(FLASH->SR & FLASH_SR_BSY); }
+
+void flash_erase_all_pages(void) {
+  FLASH->CR |= FLASH_CR_MER;
+  FLASH->CR |= FLASH_CR_STRT;
+  while (!flash_ready()) {
+  };
+  FLASH->CR &= FLASH_CR_MER;
+}
+
+void flash_erase_page(uint32_t address) {
+  FLASH->CR |= FLASH_CR_PER;
+  FLASH->AR = address;
+  FLASH->CR |= FLASH_CR_STRT;
+  while (!flash_ready()) {
+  }
+  FLASH->CR &= ~(FLASH_CR_PER);
+}
+
+void flash_write(uint32_t address, uint32_t data) {
+  FLASH->CR |= FLASH_CR_PG;
+  while (!flash_ready()) {
+  }
+  *(__IO uint16_t *)address = (uint16_t)data;
+  while (!flash_ready()) {
+  }
+  address += 2;
+  data >>= 16;
+  *(__IO uint16_t *)address = (uint16_t)data;
+  while (!flash_ready()) {
+  }
+  FLASH->CR &= ~(FLASH_CR_PG);
+}
+
 int main() {
   configure_SYSCLOCK(); // ^
 
@@ -54,16 +98,32 @@ int main() {
 
   uint16_t encoder_counter_current = 0;
   uint16_t encoder_counter_prev = 0;
+  int8_t is_read = 0;
+
+  __IO uint32_t saved_encoder_counter_value = *(__IO uint32_t*)0x08007C00;
+  if (saved_encoder_counter_value < 1000) {
+    encoder_counter_current = saved_encoder_counter_value;
+  }
+
+  TIM1->CNT &= ~(0xFFFFFF);
+  TIM1->CNT |= saved_encoder_counter_value;
 
   while (1) {
-    encoder_counter_current = TIM1->CNT;
+    if (is_read) {
+      encoder_counter_current = TIM1->CNT;
+    }
     if (encoder_counter_current != encoder_counter_prev) {
       SSD1306_GotoXY(10, 10);
       char info[100] = {0};
       sprintf(info, "Power: %d   ", encoder_counter_current);
       SSD1306_Puts(info, &Font_7x10, 1);
-
       SSD1306_UpdateScreen();
+
+      flash_unlock();
+      flash_erase_page(0x08007C00);
+      flash_write(0x08007C00, encoder_counter_current);
+      flash_lock();
+
       encoder_counter_prev = encoder_counter_current;
     }
     if (GPIOA->IDR & (0b1 << 4)) {
@@ -71,5 +131,6 @@ int main() {
     } else {
       GPIOC->ODR |= 0b1 << 13;
     }
+    is_read = 1;
   }
 }
